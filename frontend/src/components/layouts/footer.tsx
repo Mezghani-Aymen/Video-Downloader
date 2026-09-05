085413b7d3585d066a520213baae60d618eef882
Med_Aymen mezghani <mezghanimohamedaymen@gmail.com>
2026-09-05 16:54:10 +0100

    feat: add Footer component with social media links

---
+++ frontend/src/components/layouts/footer.tsx
@@ -0,0 +1,30 @@
+import { SocialLink } from "@/components/ui/socialLink";
+import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa6";
+
+
+export function Footer() {
+    return (
+        <footer className="border-t py-6 text-center text-xs text-muted-foreground bg-muted/20 mt-12">
+            <div className="container max-w-7xl mx-auto px-4 flex flex-col sm:flex-row items-center justify-between gap-4">
+                <p>© 2026 OmniStream Video Downloader. Created by <span className="text-purple-400">Mezghani Mohamed Aymen</span>.</p>
+
+                <div className="flex items-center gap-4 text-muted-foreground font-medium">
+                    <SocialLink
+                        href="https://github.com"
+                        aria="GitHub Profile"
+                        iconComponentName={FaGithub}
+                    />
+
+                    <span>•</span>
+
+                    <SocialLink
+                        href="https://github.com"
+                        aria="GitHub Profile"
+                        iconComponentName={FaLinkedin}
+                    />
+                </div>
+            </div>
+        </footer>)
+
+}
